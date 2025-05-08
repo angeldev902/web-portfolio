07d4e813aecebcc6b405b1dc83d40b8c7f7dc57f
Jose Angel <angel@dongo.mx>
2025-05-08 00:02:47 -0600

    Optimizacion de imagenes

--- src/utils/pictures.ts
+++ src/utils/pictures.ts
@@ -1,9 +1,9 @@
 //Hero 
-import hero1 from '../assets/images/portfolio/hero/hero-1.jpg';
-import hero2 from '../assets/images/portfolio/hero/hero-2.jpg';
-import hero3 from '../assets/images/portfolio/hero/hero-3.jpg';
-import picture from '../assets/images/portfolio/profile_picture.jpg';
-import projectPicture1 from '../assets/images/portfolio/projetcs.jpg'
+import hero1 from '../assets/images/portfolio/hero/hero-1.png';
+import hero2 from '../assets/images/portfolio/hero/hero-2.png';
+import hero3 from '../assets/images/portfolio/hero/hero-3.png';
+import picture from '../assets/images/portfolio/profile_picture.png';
+import projectPicture1 from '../assets/images/portfolio/projetcs.png'
 
 const portfolioImages = {
     hero: [hero1, hero2, hero3],
