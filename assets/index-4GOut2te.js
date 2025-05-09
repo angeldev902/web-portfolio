5da70afd7d017ce2152be0afc9fea643b6c09dfd
Jose Angel <angel@dongo.mx>
2025-05-08 23:41:58 -0600

    Updates

--- assets/index-4GOut2te.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-B6PKJ2VT.js";function r(){return t.jsx(t.Fragment,{children:t.jsx("h2",{children:"Contact"})})}export{r as default};
--- assets/index-Cg-YW2fu.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-B6PKJ2VT.js";function e(){return r.jsx(r.Fragment,{children:r.jsx("h2",{children:"Projects"})})}export{e as default};
