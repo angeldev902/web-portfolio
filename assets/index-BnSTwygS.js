43d6ea3152a47194e7c71960629ffeaa90c9182a
Jose Angel <angel@dongo.mx>
2025-05-11 15:27:12 -0600

    Updates

---
+++ assets/index-BnSTwygS.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CY6SisBZ.js";function r(){return t.jsx(t.Fragment,{children:t.jsx("h2",{children:"Contact"})})}export{r as default};
--- assets/index-CaWMKwHn.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DbZLzSZ-.js";function r(){return t.jsx(t.Fragment,{children:t.jsx("h2",{children:"Contact"})})}export{r as default};
